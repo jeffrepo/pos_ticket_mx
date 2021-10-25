4dc4dbc375d889faec7a0e2d939894f8ea074da4
Geordi-Castaneda <jordyorantes9@gmail.com>
2021-10-25 08:18:06 -0600

    Cambios en la vista del ticket mx

--- models/account_move.py
+++ models/account_move.py
@@ -14,39 +14,50 @@ class Picking(models.Model):
         factura_id = self.env['account.move'].search([('id','=',id_factura)])
         logging.warn("factura_id");
         logging.warn(factura_id)
-        cfdi = base64.decodestring(factura_id.l10n_mx_edi_cfdi)
-        logging.warn(cfdi)
-        # cfdi = factura_id.l10n_mx_edi_get_xml_etree(cfdi)
-        # logging.warn(cfdi)
-        xml = factura_id.l10n_mx_edi_get_xml_etree();
+        cfdi=None
+        sello_sat = None
+        # factura_id = None
+        Emisor = None
+        xml = None
+        regimen_fiscal = None
+        sello_digital_emisor = None
+        tfd = None
+        total_letras = ''
+        certificado_sat = None
+        fecha_certificacion = None
+        folio_fiscal = None
+        cadena_original = None
+        if factura_id:
+            logging.warn("Primera parte factura_id si trae algo")
+            logging.warn(factura_id)
+            cfdi = base64.decodestring(factura_id.l10n_mx_edi_cfdi)
+            xml = factura_id.l10n_mx_edi_get_xml_etree()
+            tfd = factura_id.l10n_mx_edi_get_tfd_etree(xml)
+            sello_sat = tfd.get('selloSAT', tfd.get('SelloSAT', 'No identificado'))
+            certificado_sat = tfd.get('NoCertificadoSAT')
+            folio_fiscal = tfd.get('UUID')
+            
         logging.warn("El xml")
-        logging.warn(xml);
+        logging.warn(xml)
         logging.warn("-----------------")
-        tfd = factura_id.l10n_mx_edi_get_tfd_etree(xml)
-        sello_sat = tfd.get('selloSAT', tfd.get('SelloSAT', 'No identificado'));
-        regimen_fiscal = xml.Emisor.get('RegimenFiscal', '');
-        certificado_sat = tfd.get('NoCertificadoSAT');
-        fecha_certificacion = xml.get('fecha', xml.get('Fecha', '')).replace('T', ' ');
-        folio_fiscal = tfd.get('UUID');
-        cadena_original = factura_id._get_l10n_mx_edi_cadena();
-        total_letras = factura_id.l10n_mx_edi_amount_to_text();
-        sello_digital_emisor = xml.get('sello', xml.get('Sello', 'No identificado'));
-        cadena_factura_id = factura_id.l10n_mx_edi_cfdi_uuid;
-        logging.warn("cadena_factura_id")
-        logging.warn(cadena_factura_id)
-        extraer_ultimos_digitos = cadena_factura_id[-8:]
-        logging.warn("extraer_ultimos_digitos")
-        logging.warn(extraer_ultimos_digitos)
-        logging.warn(factura_id.currency_id.decimal_places + factura_id.l10n_mx_edi_cfdi_amount)
-        # string = quote_plus('https://verificacfdi.facturaelectronica.sat.gob.mx/default.aspx?' + keep_query(
-        # re=factura_id.l10n_mx_edi_cfdi_supplier_rfc, rr=factura_id.l10n_mx_edi_cfdi_customer_rfc,
-        # tt='%.*f' % (factura_id.currency_id.decimal_places, factura_id.l10n_mx_edi_cfdi_amount), id=factura_id.l10n_mx_edi_cfdi_uuid)
-        # + '&amp;fe=%s' % quote_plus( sello, 'utf-8', 'strict', '=/').replace('%2B', '+'));
-        # string_unificado = ("https://verificacfdi.facturaelectronica.sat.gob.mx/default.aspx?re="+
-        # factura_id.l10n_mx_edi_cfdi_supplier_rfc+
-        # "rr="+factura_id.l10n_mx_edi_cfdi_customer_rfc+
-        # "tt="+ 25 +
-        # "id="+str(factura_id.l10n_mx_edi_cfdi_uuid));
+        logging.warn("Que es tfd ?")
+        logging.warn(tfd)
+
+
+
+        if xml:
+            regimen_fiscal = xml.Emisor.get('RegimenFiscal', '')
+            fecha_certificacion = xml.get('fecha', xml.get('Fecha', '')).replace('T', ' ')
+            sello_digital_emisor = xml.get('sello', xml.get('Sello', 'No identificado'))
+
+
+        if factura_id:
+            cadena_original = factura_id._get_l10n_mx_edi_cadena()
+            total_letras = factura_id.l10n_mx_edi_amount_to_text()
+            cadena_factura_id = factura_id.l10n_mx_edi_cfdi_uuid
+            extraer_ultimos_digitos = cadena_factura_id[-8:]
+
+
         datos_factura = {
             'total_string': total_letras,
             'regimen_fiscal': regimen_fiscal,
@@ -56,6 +67,16 @@ class Picking(models.Model):
             'cadena_original': cadena_original,
             'sello_sat': sello_sat,
             'sello_digital_emisor': sello_digital_emisor,
-            # 'string_unificado': string_unificado,
         }
         return datos_factura
+
+    def letras_numeros (self, id_pedido):
+        pedido = self.env['pos.order'].search([('id','=',id_pedido)])
+        string_to = self.env['account.move'].l10n_mx_edi_amount_to_text(pedido)
+        logging.warn("Pedido")
+        logging.warn(string_to)
+        # string_total = pedido.l10n_mx_edi_amount_to_text()
+        # self.env['account.move'].nombre_funcion(tu parametro en este caso tu pedido)
+        logging.warn("Total letras a numeros :D")
+        hola = "Hola"
+        return string_to
--- static/src/js/screens.js
+++ static/src/js/screens.js
@@ -16,20 +16,28 @@ screens.ReceiptScreenWidget.include({
     render_receipt: function(){
         var order = this.pos.get_order();
         var self = this;
+        // this._super();
         var dicc_varios_datos={
           'producto_descuento_id':order.pos.config.producto_descuento_id[0],
           'cliente': order.attributes.client.name,
           'vat': order.attributes.client.vat,
         };
 
+        // rpc.query({
+        //   model: 'pos.order',
+        //   method: 'buscar_pedido',
+        //   args: [[],order.name],
+
+
         rpc.query({
           model: 'pos.order',
           method: 'search_read',
-          args: [[['pos_reference', '=', order.name]], []],
+          args: [[['pos_reference', '=', order.name.toString()]], []],
         }, {
-          timeout: 5000,
+          timeout: 10000,
         }).then(function (orders){
-
+          console.log("que es orders? ::");
+          console.log(orders);
           if (orders.length > 0 && 'account_move' in orders[0] && orders[0]['account_move'].length > 0) {
 
             rpc.query({
@@ -40,25 +48,31 @@ screens.ReceiptScreenWidget.include({
             }, {
               timeout:5000,
             }).then(function (facturas){
-
               if (facturas.length > 0) {
                 rpc.query({
                   model: 'account.move',
                   method: 'obtener_valores_factura_mx',
                   args: [facturas[0]['id']],
                 }).then(function (factura_unica) {
+                  var extraida = '';
                   var receipt_env = self.get_receipt_render_env();
                   receipt_env['qr_string'] = false;
+                  receipt_env['total_letras'] = factura_unica['total_string'];
+                  order.set_totalString(factura_unica['total_string']);
                   receipt_env['receipt']['datos_certificacion'] = factura_unica;
                   receipt_env['certificado_sat'] = factura_unica['certificado_sat'];
                   receipt_env['fecha_certificacion'] = factura_unica['fecha_certificacion'];
                   receipt_env['folio_fiscal'] = factura_unica['folio_fiscal'];
-                  console.log("receipt_env");
-                  console.log(receipt_env);
                   receipt_env['producto_descuento_id']= order.pos.config.producto_descuento_id[0];
                   receipt_env['vat']= order.attributes.client.vat;
-                  let cadena = factura_unica['sello_digital_emisor']
-                  let extraida = cadena.substring(336, 346)
+                  if (factura_unica) {
+                    let cadena = factura_unica['sello_digital_emisor']
+                    if (cadena != null) {
+                        let extraida = cadena.substring(336, 346)
+                    }
+
+                  }
+
                   // "https://verificacfdi.facturaelectronica.sat.gob.mx/default.aspx?re=",factura_unica['folio_fiscal'],+"&rr="+receipt_env['receipt']['company']['vat'], +"&tt="+receipt_env['vat'], receipt_env['receipt']['total_with_tax'], extraida
                   var string_parte_1 = "&rr="+receipt_env['vat'];
                   var link = ["https://verificacfdi.facturaelectronica.sat.gob.mx/default.aspx?re=",receipt_env['receipt']['company']['vat'],string_parte_1,"&","tt=",(receipt_env['receipt']['total_with_tax']).toFixed(2),"&","id=",factura_unica['folio_fiscal'],"&","fe=",extraida].join('');
@@ -69,23 +83,66 @@ screens.ReceiptScreenWidget.include({
                   receipt_env['qr_string'] = link;
 
                   self.$('.pos-receipt-container').html(QWeb.render('OrderReceipt', receipt_env));
+                  console.log("receipt_env");
                   console.log(receipt_env);
                 });
 
 
               }
 
+              //else rpc a pos.order buscando la funcion a letras
+              //Codigo python abajo
+              //self.env['account.move'].nombre_funcion(tu parametro en este caso tu pedido)
             });
 
           }
+          else{
 
+            rpc.query({
+              model: 'pos.order',
+              method: 'texto_total',
+              args: [[],order.get_total_with_tax()],
+            },{
+              timeout: 5000,
+            }).then(function(total){
+              var receipt_env = self.get_receipt_render_env();
+              receipt_env['total_letras'] = total;
+              order.set_totalString(total);
+              console.log("order.get_totalString()");
+              console.log(order.get_totalString());
+              self.$('.pos-receipt-container').html(QWeb.render('OrderReceipt', receipt_env));
+              console.log("El total en letras funcion correcta");
+              console.log(total);
+              console.log("que es receipt_env");
+              console.log(receipt_env);
+            });
+
+          }
         });
-
-        console.log(dicc_varios_datos);
-
+        this._super();
     }
-})
+});
 
+var _super_order = models.Order.prototype;
 
+models.Order = models.Order.extend({
+
+  set_totalString: function(totalString){
+    this.set({
+      totalString: totalString
+    });
+  },
+
+  get_totalString: function(){
+    return this.get('totalString');
+  },
+
+  initialize: function() {
+    _super_order.initialize.apply(this,arguments);
+    this.set_totalString();
+
+  },
+
+});
 
 });
