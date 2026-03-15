5700618557eb83eddeb9d698fc2e659a69011218
jeffrepo <jeffer.leo93@gmail.com>
2026-03-15 15:22:10 -0600

    19.0 test

--- __manifest__.py
+++ __manifest__.py
@@ -20,19 +20,19 @@
         #'views/templates.xml',
     ],
     'assets':{
-        'point_of_sale.assets': [
-            'pos_ticket_mx/static/src/css/pos_ticket_mx.css',
-            'pos_ticket_mx/static/src/js/qrcode.js',
-            # 'pos_ticket_mx/static/src/js/Screens/PaymentScreen/PaymentScreen.js',
-            'pos_ticket_mx/static/src/js/Screens/ReceiptScreen/OrderReceipt.js',
-            'pos_ticket_mx/static/src/js/models.js',
-        ],
-        'web.assets_qweb':[
-            'pos_ticket_mx/static/src/xml/**/*',
-        ],
+        # 'point_of_sale.assets': [
+        #     'pos_ticket_mx/static/src/css/pos_ticket_mx.css',
+        #     'pos_ticket_mx/static/src/js/qrcode.js',
+        #     'pos_ticket_mx/static/src/js/Screens/ReceiptScreen/OrderReceipt.js',
+        #     'pos_ticket_mx/static/src/js/models.js',
+        # ],
+        # 'web.assets_qweb':[
+        #     'pos_ticket_mx/static/src/xml/**/*',
+        # ],
     },
     'license': 'LGPL-3',
 
     'installable': True,
     'auto_install': False,
 }
+
