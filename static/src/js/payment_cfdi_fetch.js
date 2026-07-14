68ff6522d27000305119bddb9292af2e400e1739
Jefferson Silva <jeffer.leo93@gmail.com>
2026-07-14 14:18:09 -0600

    Usar carga simple de CFDI al imprimir

--- static/src/js/payment_cfdi_fetch.js
+++ static/src/js/payment_cfdi_fetch.js
@@ -3,9 +3,6 @@
 import { patch } from "@web/core/utils/patch";
 import { PaymentScreen } from "@point_of_sale/app/screens/payment_screen/payment_screen";
 import { PosStore } from "@point_of_sale/app/services/pos_store";
-import OrderPaymentValidation from "@point_of_sale/app/utils/order_payment_validation";
-
-const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 async function loadMxCfdiTicketData(pos, order) {
     if (!order || order.isMxInvoiceOnline?.()) {
@@ -23,25 +20,22 @@ async function loadMxCfdiTicketData(pos, order) {
         order.id,
     ].filter((identifier, index, array) => identifier && array.indexOf(identifier) === index);
 
-    for (let attempt = 0; attempt < 10; attempt++) {
-        for (const identifier of identifiers) {
-            try {
-                const orm = pos.env?.services?.orm;
-                const data = orm
-                    ? await orm.call("pos.order", "get_mx_cfdi_ticket_data_by_uuid", [identifier])
-                    : await pos.data.call("pos.order", "get_mx_cfdi_ticket_data_by_uuid", [identifier]);
-                if (data?.barcode_src || data?.extra_values?.barcode_src) {
-                    order.mx_cfdi = data;
-                    return;
-                }
-                if (data && Object.keys(data).length) {
-                    order.mx_cfdi = data;
-                }
-            } catch (e) {
-                // No rompas el flujo si falla obtener datos.
+    for (const identifier of identifiers) {
+        try {
+            const orm = pos.env?.services?.orm;
+            const data = orm
+                ? await orm.call("pos.order", "get_mx_cfdi_ticket_data_by_uuid", [identifier])
+                : await pos.data.call("pos.order", "get_mx_cfdi_ticket_data_by_uuid", [identifier]);
+            if (data?.barcode_src || data?.extra_values?.barcode_src) {
+                order.mx_cfdi = data;
+                return;
             }
+            if (data && Object.keys(data).length) {
+                order.mx_cfdi = data;
+            }
+        } catch (e) {
+            // No rompas el flujo si falla obtener datos.
         }
-        await wait(800);
     }
 }
 
@@ -70,13 +64,6 @@ patch(PaymentScreen.prototype, {
             await super._finalizeValidation(...arguments);
         }
         await loadMxCfdiTicketData(this.pos, this.currentOrder);
-    },
-});
-
-patch(OrderPaymentValidation.prototype, {
-    async afterOrderValidation() {
-        await loadMxCfdiTicketData(this.pos, this.order);
-        return await super.afterOrderValidation(...arguments);
     },
 });
 
